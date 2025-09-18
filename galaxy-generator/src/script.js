82c68655602c3527cab242c92d4f834a9660bac5
mariamoniztome <maria.tome@builtcolab.pt>
2025-09-18 09:27:13 +0100

    Add branches

--- galaxy-generator/src/script.js
+++ galaxy-generator/src/script.js
@@ -14,14 +14,84 @@ const canvas = document.querySelector('canvas.webgl')
 // Scene
 const scene = new THREE.Scene()
 
-/**
- * Test cube
- */
-const cube = new THREE.Mesh(
-    new THREE.BoxGeometry(1, 1, 1),
-    new THREE.MeshBasicMaterial()
-)
-scene.add(cube)
+// /**
+//  * Test cube
+//  */
+// const cube = new THREE.Mesh(
+//     new THREE.BoxGeometry(1, 1, 1),
+//     new THREE.MeshBasicMaterial()
+// )
+// scene.add(cube)
+
+const parameters = {}
+parameters.count = 10000
+parameters.size = 0.01
+parameters.radius = 5
+parameters.branches = 5
+parameters.spin = 1
+
+let points = null
+let geometry = null
+let material = null
+
+const generateGalaxy = () => {
+    // Destroy old galaxy
+    if(points !== null)
+    {
+        geometry.dispose()
+        material.dispose()
+        scene.remove(points)
+    }
+
+    // Geometry
+    geometry = new THREE.BufferGeometry()
+    const positions = new Float32Array(parameters.count * 3)
+
+    for(let i = 0; i < parameters.count; i++)
+    {
+        const i3 = i * 3
+
+        const radius = Math.random() * parameters.radius
+        const branchAngle = (i % parameters.branches) / parameters.branches * Math.PI * 2
+
+        if (i < 20 )  {
+
+        }
+
+        positions[i3 + 0] = Math.cos(branchAngle) * radius
+        positions[i3 + 1] = 0
+        positions[i3 + 2] = Math.sin(branchAngle) * radius
+    }
+    geometry.setAttribute('position', new THREE.BufferAttribute(positions, 3))
+    
+    // Material
+    material = new THREE.PointsMaterial({
+        size: parameters.size,
+        sizeAttenuation: true, // isto faz com que o tamanho varie consoante a distância
+        depthWrite: false, // isto faz com que as partículas não se sobreponham
+        blending: THREE.AdditiveBlending, // isto faz com que as partículas se somem em vez de se anularem quando se sobrepõem
+    })
+
+    // Points
+    points = new THREE.Points(geometry, material)
+    scene.add(points)
+}
+
+generateGalaxy()
+
+// Tweaks
+gui.add(parameters, 'count').min(100).max(1000000).step(100).onFinishChange(() => { 
+    generateGalaxy()
+})
+gui.add(parameters, 'size').min(0.001).max(0.1).step(0.001).onFinishChange(() => {  
+    generateGalaxy()
+})
+gui.add(parameters, 'radius').min(0.01).max(20).step(0.01).onFinishChange(() => { 
+    generateGalaxy()
+})
+gui.add(parameters, 'branches').min(2).max(20).step(1).onFinishChange(() => { 
+    generateGalaxy()
+})
 
 /**
  * Sizes
@@ -66,6 +136,7 @@ controls.enableDamping = true
 const renderer = new THREE.WebGLRenderer({
     canvas: canvas
 })
+
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 
