8e1ee14b07035232a01cedff8645ab4d0e9f485b
mariamoniztome <maria.tome@builtcolab.pt>
2025-09-25 15:11:58 +0100

    add mouse hover event

--- raycast-mouse-events/src/script.js
+++ raycast-mouse-events/src/script.js
@@ -66,6 +66,29 @@ window.addEventListener('resize', () =>
     renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 })
 
+// Mouse
+const mouse = new THREE.Vector2()
+
+window.addEventListener('mousemove', (event) =>
+{
+    mouse.x = (event.clientX / sizes.width) * 2 - 1
+    mouse.y = - (event.clientY / sizes.height) * 2 + 1  
+    // Cast a ray
+    raycaster.setFromCamera(mouse, camera)
+
+    const objectsToTest = [object1, object2, object3]
+    const intersects = raycaster.intersectObjects(objectsToTest)
+
+    for(const object of objectsToTest)
+    {
+        object.material.color.set('#ff0000')
+    }
+    for(const intersect of intersects)
+    {
+        intersect.object.material.color.set('#0197ff')
+    }
+})
+
 /**
  * Camera
  */
@@ -102,23 +125,23 @@ const tick = () =>
     object3.position.y = Math.sin(elapsedTime * 1.1 + Math.PI) * 1.5
 
     // Cast a ray
-    const rayOrigin = new THREE.Vector3(-3, 0, 0)
-    const rayDirection = new THREE.Vector3(1, 0, 0)
-    rayDirection.normalize()
+    // const rayOrigin = new THREE.Vector3(-3, 0, 0)
+    // const rayDirection = new THREE.Vector3(1, 0, 0)
+    // rayDirection.normalize()
 
-    raycaster.set(rayOrigin, rayDirection)
+    // raycaster.set(rayOrigin, rayDirection)
 
-    const objectsToTest = [object1, object2, object3]
-    const intersects = raycaster.intersectObjects(objectsToTest)
+    // const objectsToTest = [object1, object2, object3]
+    // const intersects = raycaster.intersectObjects(objectsToTest)
 
-    for(const object of objectsToTest)
-    {
-        object.material.color.set('#ff0000')
-    }
-    for(const intersect of intersects)
-    {
-        intersect.object.material.color.set('#0000ff')
-    }
+    // for(const object of objectsToTest)
+    // {
+    //     object.material.color.set('#ff0000')
+    // }
+    // for(const intersect of intersects)
+    // {
+    //     intersect.object.material.color.set('#0000ff')
+    // }
 
     // Update controls
     controls.update()
