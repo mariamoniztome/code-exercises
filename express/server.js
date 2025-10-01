a6436cd2bafce2f83f36fb61bf66b72e69a263dc
mariamoniztome <maria.tome@builtcolab.pt>
2025-10-01 16:22:46 +0100

    Add query param

--- express/server.js
+++ express/server.js
@@ -1,17 +1,70 @@
-import express from 'express';
-import startups from './data/data.js'
+import express from 'express'
+import { startups } from './data/data.js'
 
-// Create a new express application instance
-const app = express();
+const PORT = 8000
 
-console.log(app); // Express gives us many methods to work with
+const app = express()
 
-// This is a method app.get() that takes two arguments
-// 1. The path for which we want to trigger this method
-// 2. A callback function that will be called when the path is matched
 app.get('/api', (req, res) => {
-    res.json(startups);
+
+  let filteredData = startups
+
+  const { industry, country, continent, is_seeking_funding, has_mvp } = req.query
+
+  if (industry) {
+    filteredData = filteredData.filter( startup => 
+      startup.industry.toLowerCase() === industry.toLowerCase()
+    )
+  }
+
+  if (country) {
+    filteredData = filteredData.filter( startup => 
+      startup.country.toLowerCase() === country.toLowerCase()
+    )
+  }
+  
+  if (continent) {
+    filteredData = filteredData.filter( startup => 
+      startup.continent.toLowerCase() === continent.toLowerCase()
+    )
+  }
+
+  if (is_seeking_funding) {
+    filteredData = filteredData.filter( startup => 
+      startup.is_seeking_funding === JSON.parse(is_seeking_funding.toLowerCase())
+    )
+  }
+  
+  if (has_mvp) {
+    filteredData = filteredData.filter( startup => 
+      startup.has_mvp === JSON.parse(has_mvp.toLowerCase())
+    )
+  }
+
+
+  res.json(filteredData)
+
 })
 
-// Define a route handler for the default home page
-app.listen(8000, () => console.log(`Server is running at http://localhost:8000`));
+app.listen(PORT, () => console.log(`server connected on port ${PORT}`))
+
+
+/*
+Challenge:
+1. When a user hits the /api endpoint with query params, filter the data so 
+we only serve objects that meet their requirements. 
+     
+The user can filter by the following properties:
+  industry, country, continent, is_seeking_funding, has_mvp
+
+Test Cases
+
+/api?industry=renewable%20energy&country=germany&has_mvp=true
+  Should get the "GreenGrid Energy" object.
+
+/api?industry=renewable%20energy&country=germany&has_mvp=false
+  Should not get any object
+
+/api?continent=asia&is_seeking_funding=true&has_mvp=true
+  should get for objects with IDs 3, 22, 26, 29
+*/
