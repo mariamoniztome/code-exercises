b8c7bc8be5a873aa80b6cb642df50ac5601771f6
mariamoniztome <maria.tome@builtcolab.pt>
2025-10-01 15:29:18 +0100

    Create Server

--- express/server.js
+++ express/server.js
@@ -1,1 +1,11 @@
-console.log('We have a package.json')
+import express from 'express';
+
+const PORT = 8000;
+
+// Create a new express application instance
+const app = express();
+
+// Define a route handler for the default home page
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
+});
