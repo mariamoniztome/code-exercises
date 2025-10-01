2835e06d978f63034d91adf828758d6935147b9a
mariamoniztome <maria.tome@builtcolab.pt>
2025-10-01 16:02:35 +0100

    Add data file and create get

--- express/server.js
+++ express/server.js
@@ -1,11 +1,17 @@
 import express from 'express';
-
-const PORT = 8000;
+import startups from './data/data.js'
 
 // Create a new express application instance
 const app = express();
 
+console.log(app); // Express gives us many methods to work with
+
+// This is a method app.get() that takes two arguments
+// 1. The path for which we want to trigger this method
+// 2. A callback function that will be called when the path is matched
+app.get('/api', (req, res) => {
+    res.json(startups);
+})
+
 // Define a route handler for the default home page
-app.listen(PORT, () => {
-  console.log(`Server is running at http://localhost:${PORT}`);
-});
+app.listen(8000, () => console.log(`Server is running at http://localhost:8000`));
