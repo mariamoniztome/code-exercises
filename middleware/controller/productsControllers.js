cee22dd5214ae3662e0dc33d079addccf265169f
mariamoniztome <maria.tome@builtcolab.pt>
2025-10-02 11:50:15 +0100

    Add filtering for products

--- middleware/controller/productsControllers.js
+++ middleware/controller/productsControllers.js
@@ -1,37 +1,38 @@
-import { getDBConnection } from '../database/db.js'
+import { getDBConnection } from "../database/db.js";
 
 export async function getGenres(req, res) {
-
   try {
+    const db = await getDBConnection();
 
-    const db = await getDBConnection()
-
-    const genreRows = await db.all('SELECT DISTINCT genre FROM products')
-    const genres = genreRows.map(row => row.genre)
-    res.json(genres)
-
-/*
-Challenge:
-
-1. Get all distinct genres (no repeats) from the products table.
-
-  - Our front end code is expecting an array of genres as strings, but you will likely get an array of objects from the database. Find a solution to that!
-
-2. Serve the array of genres and open up the mini browser to check the dropdown is populated.
-
-hint.md for help  
-*/  
-
+    const genreRows = await db.all("SELECT DISTINCT genre FROM products");
+    const genres = genreRows.map((row) => row.genre);
+    res.json(genres);
   } catch (err) {
-
-    res.status(500).json({error: 'Failed to fetch genres', details: err.message})
-
+    res
+      .status(500)
+      .json({ error: "Failed to fetch genres", details: err.message });
   }
-
 }
 
-export async function getProducts() {
+export async function getProducts(req, res) {
+  try {
+    const db = await getDBConnection();
 
-  console.log('products')
+    // let productRows = await db.all('SELECT * FROM products')
+    let query = "SELECT * FROM products";
+    let params = [];
+    const { genre } = req.query;
+    if (genre) {
+      query += " WHERE genre = ?";
+      params.push(genre);
+      console.log(params);
+    }
+    const feed = await db.all(query, params);
 
+    res.json(feed);
+  } catch (err) {
+    res
+      .status(500)
+      .json({ error: "Failed to fetch products", details: err.message });
+  }
 }
