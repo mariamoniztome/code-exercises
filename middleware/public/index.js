5e4d2e270ddf231870b408279fa6207a4cafac0e
mariamoniztome <maria.tome@builtcolab.pt>
2025-10-02 11:30:45 +0100

    Feed dropdown

--- middleware/public/index.js
+++ middleware/public/index.js
@@ -54,7 +54,7 @@ init()
  */
 async function populateGenreSelect() {
   const res = await fetch('/api/products/genres')
-  const genres = await res.json()
+  const genres = await res.json() // expects an array of genres as strings: ['rock', 'pop', ...]
   const select = document.getElementById('genre-select')
 
   genres.forEach(genre => {
