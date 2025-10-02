4f9ac4431ba58fc1ccb40cbe947d7ff1c69d0306
mariamoniztome <maria.tome@builtcolab.pt>
2025-10-02 09:53:08 +0100

    Add static

--- middleware/server.js
+++ middleware/server.js
@@ -1,6 +1,19 @@
+import e from 'express'
 import express from 'express'
 
 const app = express() 
+
+// app.use((req, res, next)=>{
+//     console.log('middleware')
+//     next()
+// })
+
+// app.use((req, res, next)=>{
+//     console.log(`[${new Date().toISOString()}] ${req.method} ${req.url}`)
+//     next()
+// })
+
+app.use(express.static('public'))
 
 app.get('/', (req, res) => {
     res.send('<!doctype html><html><body>Hello Express!</body></html>')
