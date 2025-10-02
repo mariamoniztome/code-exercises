65db371f5e25df3ddcb6a41a308e301312bd99a6
mariamoniztome <maria.tome@builtcolab.pt>
2025-10-02 11:55:36 +0100

    Add search funcionality

--- middleware/controller/productsControllers.js
+++ middleware/controller/productsControllers.js
@@ -1,38 +1,42 @@
 import { getDBConnection } from "../database/db.js";
 
 export async function getGenres(req, res) {
-  try {
-    const db = await getDBConnection();
+    try {
+        const db = await getDBConnection();
 
-    const genreRows = await db.all("SELECT DISTINCT genre FROM products");
-    const genres = genreRows.map((row) => row.genre);
-    res.json(genres);
-  } catch (err) {
-    res
-      .status(500)
-      .json({ error: "Failed to fetch genres", details: err.message });
-  }
+        const genreRows = await db.all("SELECT DISTINCT genre FROM products");
+        const genres = genreRows.map((row) => row.genre);
+        res.json(genres);
+    } catch (err) {
+        res
+            .status(500)
+            .json({ error: "Failed to fetch genres", details: err.message });
+    }
 }
 
 export async function getProducts(req, res) {
-  try {
-    const db = await getDBConnection();
+    try {
+        const db = await getDBConnection();
 
-    // let productRows = await db.all('SELECT * FROM products')
-    let query = "SELECT * FROM products";
-    let params = [];
-    const { genre } = req.query;
-    if (genre) {
-      query += " WHERE genre = ?";
-      params.push(genre);
-      console.log(params);
+        // let productRows = await db.all('SELECT * FROM products')
+        let query = "SELECT * FROM products";
+        let params = [];
+        const { genre, search } = req.query;
+        if (genre) {
+            query += " WHERE genre = ?";
+            params.push(genre);
+            console.log(params);
+        } else if (search) {
+            query += " WHERE title LIKE ? OR artist LIKE ? OR genre LIKE ?";
+            const searchPattern = `%${search}%`;
+            params.push(searchPattern, searchPattern, searchPattern);
+        }
+        const feed = await db.all(query, params);
+
+        res.json(feed);
+    } catch (err) {
+        res
+            .status(500)
+            .json({ error: "Failed to fetch products", details: err.message });
     }
-    const feed = await db.all(query, params);
-
-    res.json(feed);
-  } catch (err) {
-    res
-      .status(500)
-      .json({ error: "Failed to fetch products", details: err.message });
-  }
 }
