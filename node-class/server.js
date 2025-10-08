6a3b62f344384e0970dd695be88a7bab4ec5f179
mariamoniztome <maria.tome@builtcolab.pt>
2025-10-08 10:27:11 +0100

    Add fac exercises

---
+++ node-class/server.js
@@ -0,0 +1,12 @@
+const PORT = 3000;
+const HOST = '127.0.0.1'
+const http = require("http");
+
+const httpServer = http.createServer((req, res) => {
+  res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
+  res.end("Hello world");
+});
+
+httpServer.listen(PORT, HOST,  () => {
+  console.log(`Server is running on port ${PORT}, no host ${HOST}`);
+});
