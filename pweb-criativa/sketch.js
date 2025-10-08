7bcd3152e89d654da154d06e71b2c63ef9a3c039
mariamoniztome <maria.tome@builtcolab.pt>
2025-10-08 20:09:53 +0100

    New exercises

--- pweb-criativa/sketch.js
+++ pweb-criativa/sketch.js
@@ -1,16 +1,18 @@
 function setup() {
-  createCanvas(400, 400, WEBGL);
+  createCanvas(500, 500);
+  color = color(255, 255, 255);
 }
 
 let isLit = false;
 
 function draw() {
-  background(220);
-  let c = color(255,192,203);
-  fill(c)
-  noStroke();
-  ambientLight(255, 0, 255);
+  background(149, 175, 221)
+  ellipse(250, 300, 200, 300)
+  fill(0, 0, 0)
+  ellipse(250, 300, 200, 300)
+  fill(color)
+  circle(250, 200, 180) 
+  fill(255, 0, 0)
+  noStroke()
 
-  orbitControl();
-  torus(30, 20);
 }
